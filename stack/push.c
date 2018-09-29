cabedc2c16dfda73b208a31ab0bc33fb1bbf610b
imamsutono <imamssutono@gmail.com>
2018-09-30 06:27:52 +0700

    Add pop, isempty, and isfull method

--- stack/push.c
+++ stack/push.c
@@ -4,12 +4,38 @@ int MAXSIZE = 8;
 int stack[8];
 int top = -1;
 
+int isempty() {
+	if (top == -1)
+		return 1;
+	else
+		return 0;
+}
+
+int isfull() {
+	if (top == MAXSIZE)
+		return 1;
+	else
+		return 0;
+}
+
 int push(int data) {
 	if (!isfull()) {
 		top = top + 1;
 		stack[top] = data;
 	} else {
-		printf("Could not insert data, stack is full.\n");
+		printf("Tidak dapat menambahkan data, stack sudah penuh.\n");
+	}
+}
+
+int pop() {
+	int data;
+
+	if(!isempty()) {
+		data = stack[top];
+		top = top - 1;
+		return data;
+	} else {
+		printf("Tidak dapat mengambil, stack masih kosong\n");
 	}
 }
 
